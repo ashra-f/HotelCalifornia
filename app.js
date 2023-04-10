1499b43eed4070ec7223eac221b154901a484064
“ashra-f” <appletreemann@protonmail.com>
2023-04-10 18:09:15 -0400

    Heroku db v5

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
-const { connection, createAmenitiesTable, createCustomersTable, createReservationsTable, createRoomsTable, createRoomImgsTable, insertAmenities, insertRoomImgs, insertRoomsData } = require("./config/db");
+const { connection, createAmenitiesTable, createCustomersTable, 
+  createReservationsTable, createRoomsTable, createRoomImgsTable } = require("./config/db");
 
 // Connect to database
 connection.connect((err) => {
@@ -20,26 +21,6 @@ connection.connect((err) => {
   await createReservationsTable();
   await createRoomsTable();
   await createRoomImgsTable();
-  // await insertRoomImgs();
-
-//   const roomsData = [
-//   { roomId: 1, description: 'Indulge in the ultimate comfort and relaxation in our luxurious single bed room, featuring high-end amenities.', size: 'Standard Single', price_per_night: 50, max_guests: 1, availability: 'available' },
-//   { roomId: 2, description: 'Indulge in the ultimate comfort and relaxation in our luxurious single bed room, featuring high-end amenities.', size: 'Deluxe Single', price_per_night: 75, max_guests: 1, availability: 'not available' },
-//   { roomId: 3, description: 'Indulge in the ultimate comfort and relaxation in our luxurious single bed room, featuring high-end amenities.', size: 'Executive Single', price_per_night: 90, max_guests: 1, availability: 'available' },
-//   { roomId: 4, description: 'Experience the height of luxury with our spacious double room, complete with stylish decor and premium amenities.', size: 'Standard Double', price_per_night: 100, max_guests: 2, availability: 'available' },
-//   { roomId: 5, description: 'Experience the height of luxury with our spacious double room, complete with stylish decor and premium amenities.', size: 'Deluxe Double', price_per_night: 85, max_guests: 2, availability: 'available' },
-//   { roomId: 6, description: 'Experience the height of luxury with our spacious double room, complete with stylish decor and premium amenities.', size: 'Executive Double', price_per_night: 100, max_guests: 2, availability: 'available' },
-//   { roomId: 7, description: 'Escape to pure luxury with our exquisite suite, featuring a separate living area, and stunning views of the city.', size: 'Standard Suite', price_per_night: 350, max_guests: 7, availability: 'available' },
-//   { roomId: 8, description: 'Escape to pure luxury with our exquisite suite, featuring a separate living area, and stunning views of the city.', size: 'Deluxe Suite', price_per_night: 400, max_guests: 7, availability: 'available' },
-//   { roomId: 9, description: 'Escape to pure luxury with our exquisite suite, featuring a separate living area, and stunning views of the city.', size: 'Executive Suite', price_per_night: 450, max_guests: 7, availability: 'available' },
-//   { roomId: 10, description: 'Reserve a professional conference room, complete with state-of-the-art equipment and comfortable seating to ensure success.', size: 'Standard Conference', price_per_night: 600, max_guests: 10, availability: 'available' },
-//   { roomId: 11, description: 'Reserve a professional conference room, complete with state-of-the-art equipment and comfortable seating to ensure success.', size: 'Deluxe Conference', price_per_night: 650, max_guests: 10, availability: 'available' },
-//   { roomId: 12, description: 'Reserve a professional conference room, complete with state-of-the-art equipment and comfortable seating to ensure success.', size: 'Executive Conference', price_per_night: 700, max_guests: 10, availability: 'available' },
-// ];
-
-
-//   await insertRoomsData(roomsData);
-  // await insertAmenities(amenitiesData);
 })().catch((err) => {
   console.error("Error creating tables:", err);
 });
