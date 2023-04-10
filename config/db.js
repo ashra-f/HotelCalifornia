fe371ff7d8bfb4701c38e7e9e68d5bd57b6d85a0
“ashra-f” <appletreemann@protonmail.com>
2023-04-10 16:38:17 -0400

    Prepare for Heroku deployment 2

--- config/db.js
+++ config/db.js
@@ -1,42 +1,58 @@
+// const mysql = require("mysql2");
+
+// // Create Connection
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "ash",
+//   password: "Shampoo5=Feline=Darkness",
+//   database: "hotelcalifornia",
+// });
+
+// // // Create tables
+// // const createTableCustomer = () => {
+// //   let customerTable =
+// //     "CREATE TABLE IF NOT EXISTS table_name(email varchar(25),fname varchar(25),lname varchar(25),phone varchar(64) );";
+
+// //   connection.query(customerTable, function (err, result) {
+// //     if (err) {
+// //       console.log(err);
+// //     }
+// //     console.log("Table created");
+// //   });
+// // };
+
+// // const dropTableCustomers = () => {
+// //   let customerTable = "drop table IF EXISTS table_name";
+
+// //   connection.query(customerTable, function (err, result) {
+// //     if (err) {
+// //       console.log(err);
+// //       console.log("table does not exist");
+// //     }
+// //     console.log("table dropped");
+// //   });
+// // };
+
+// module.exports = { connection };
+
+
 const mysql = require("mysql2");
+const url = require('url');
+
+const CLEARDB_DATABASE_URL = process.env.CLEARDB_DATABASE_URL || 'mysql://ash:Shampoo5%3DFeline%3DDarkness@localhost:3306/hotelcalifornia';
+
+const parsedUrl = new url.URL(CLEARDB_DATABASE_URL);
+const hostname = parsedUrl.hostname;
+const username = parsedUrl.username;
+const password = parsedUrl.password;
+const database = parsedUrl.pathname.substring(1); // Remove the leading "/"
 
 // Create Connection
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "ash",
-  password: "Shampoo5=Feline=Darkness",
-  database: "hotelcalifornia",
+  host: hostname,
+  user: username,
+  password: password,
+  database: database,
 });
 
-// Create tables
-const createTableCustomer = () => {
-  let customerTable =
-    "CREATE TABLE IF NOT EXISTS table_name(email varchar(25),fname varchar(25),lname varchar(25),phone varchar(64) );";
-
-  connection.query(customerTable, function (err, result) {
-    if (err) {
-      console.log(err);
-    }
-    console.log("Table created");
-  });
-};
-
-const dropTableCustomers = () => {
-  let customerTable = "drop table IF EXISTS table_name";
-
-  connection.query(customerTable, function (err, result) {
-    if (err) {
-      console.log(err);
-      console.log("table does not exist");
-    }
-    console.log("table dropped");
-  });
-};
-
-// todo add create reservation
-// check if room is reserved
-// todo add rooms
-// todo amenities
-// todo reservations
-
-module.exports = { connection, createTableCustomer, dropTableCustomers };
+module.exports = { connection };
